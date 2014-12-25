bf10dd3dca0032f8dc28515d662b780a4eef3fc9
Boolky Bear <boolkybear@gmail.com>
2014-12-25 14:36:14 +0100

    [ChromaProjectApp] - Create initial storyboard

---
+++ ChromaProjectApp/ChromaProjectApp/ContainerController.swift
@@ -0,0 +1,35 @@
+//
+//  ContainerController.swift
+//  ChromaProjectApp
+//
+//  Created by Boolky Bear on 25/12/14.
+//  Copyright (c) 2014 ByBDesigns. All rights reserved.
+//
+
+import UIKit
+
+class ContainerController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
